a8f8451b81151dd9ebb698bc9d1782884a45ff20
Amine Mansour <mansour4955amine@gmail.com>
2023-11-16 20:05:43 +0100

    Update11

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import db from "./firebase";
 import { Link } from "react-router-dom";
-const SidebarChat = ({ addNewChat, name,id }) => {
+const SidebarChat = ({ addNewChat, name, id }) => {
   const createChat = () => {
     const roomName = prompt("Please enter name for chat");
-    if (roomName){
-        db.collection('rooms').add({
-          name: roomName,
-        })
+    if (roomName) {
+      db.collection("rooms").add({
+        name: roomName,
+      });
     }
   };
+  const [messages, setMessages] = useState("");
+  useEffect(() => {
+    if (id) {
+      db.collection("rooms")
+        .doc(id)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+    }
+  }, []);
   return !addNewChat ? (
     <Link to={`/rooms/${id}`}>
-    <div className="sidebarChat flex p-5 cursor-pointer border-b border-b-[#f6f6f6] hover:bg-[#ebebeb] duration-100">
-      <AccountCircleIcon />
-      <div className="sidebarChatInfo ml-4">
-        <h2 className="text-base mb-2 font-bold">{name}</h2>
-        <p>Last message...</p>
+      <div className="sidebarChat flex p-5 cursor-pointer border-b border-b-[#f6f6f6] hover:bg-[#ebebeb] duration-100">
+        <AccountCircleIcon />
+        <div className="sidebarChatInfo ml-4">
+          <h2 className="text-base mb-2 font-bold">{name}</h2>
+          <p>{messages[0]?.message}</p>
+        </div>
       </div>
-    </div>
     </Link>
   ) : (
     <div
