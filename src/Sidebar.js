a73b8f33bb993f285b8ddaf1a56820e8c6941d00
Amine Mansour <mansour4955amine@gmail.com>
2023-11-16 17:51:10 +0100

    Update8

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,26 +6,32 @@ import MoreVertIcon from "@mui/icons-material/MoreVert";
 import SearchIcon from "@mui/icons-material/Search";
 import SidebarChat from "./SidebarChat";
 import db from "./firebase";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+  const user = useSelector(state=>state.message.user.user)
   const [rooms, setRooms] = useState([]);
-  useEffect(() => {
-    const unsubscribe = db.collection("rooms").onSnapshot((snapshot) =>
-      setRooms(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          data: doc.data(),
-        }))
-      )
-    );
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+  useEffect(()=>{
+ const unsubscribe = db.collection("rooms").onSnapshot(snapshot => (
+  setRooms(snapshot.docs.map(doc => 
+    ({
+      id: doc.id,
+      data: doc.data(),
+
+    })
+    ))
+)
+);
+return ()=> {
+  unsubscribe()
+};
+  },[])
   return (
     <div className="flex-[0.35] flex flex-col">
       <div className="sidebarHeader flex justify-between p-5 border-r border-r-gray-300 items-center">
-        <AccountCircleIcon />
+        {
+          !user ? <AccountCircleIcon /> : (<img className="h-10 w-10 rounded-full shadow-shadowButton shadow-gray-950" src={user.photoURL} alt=""/>)
+        }
         <div className="sidebarHeaderRight flex items-center min-w-[10vw] justify-between gap-2">
           <span className="p-1 rounded-full bg-white active:bg-[#ededed] shadow-shadowButton shadow-gray-950 duration-100">
             <DonutLargeIcon />
@@ -52,10 +58,12 @@ const Sidebar = () => {
       </div>
       <div className="sidebarChats bg-white flex-1 overflow-y-scroll">
         <SidebarChat addNewChat />
-        {rooms.map((room) => (
-          <SidebarChat key={room.id} id={room.id} name={room.data.name} />
-        ))}
-      </div>
+        {
+          rooms.map(room => (
+            <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+          ))
+        }
+      </div> 
     </div>
   );
 };
